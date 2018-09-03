e390e5285fc12ee5d038af8b361177b6a6b4c447
Philip Washington Sorst <philip@sorst.net>
2018-09-03 18:02:45 +0200

    PHP 7.2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t philipsorst/php-testing:16-04-7-1 .
-docker push philipsorst/php-testing:16-04-7-1
+docker build -t philipsorst/php-testing:16-04-7-2 .
+docker push philipsorst/php-testing:16-04-7-2
