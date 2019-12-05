6cb5e85eaa8992999ca33f38ef5bc7d273aeb688
Philip Washington Sorst <philip@sorst.net>
2019-12-05 11:32:24 +0100

    Fixing tagging

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t philipsorst/php-testing:18-04-7-3 .
-docker push philipsorst/php-testing:18-04-7-3
+docker build -t philipsorst/php-testing:18-04-7-4 .
+docker push philipsorst/php-testing:18-04-7-4
